af8e34ee660cd128f7c47ce5da129672d1ee0638
sang-kipkoech <kipkoech@untapped-global.com>
2023-09-24 10:41:27 +0300

    feat: add jenkins scripts

---
+++ jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo apt update -y
+
+sudo apt upgrade -y
+
+sudo apt install openjdk-17-jre -y
+
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update -y
+
+sudo apt install jenkins -y
